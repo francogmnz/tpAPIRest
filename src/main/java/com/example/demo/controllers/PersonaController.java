625484aa06e6244c414a3637b7a0c6e39a116bd4
Franco <fgimenez185@gmail.com>
2023-10-10 23:00:23 -0300

    agregue los controladores

--- src/main/java/com/example/demo/controllers/PersonaController.java
+++ src/main/java/com/example/demo/controllers/PersonaController.java
@@ -2,13 +2,30 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.Persona;
 import com.example.demo.services.PersonaServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/personas")
 public class PersonaController extends BaseControllerImpl<Persona, PersonaServiceImpl> {
-
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam String filtro){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente mas tarde.\"}");
+        }
+    }
+    @GetMapping("/searchpaged")
+    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro,pageable));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente mas tarde.\"}");
+        }
+    }
 }
